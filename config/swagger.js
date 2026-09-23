d666c5bb89f7326749b3ad9bbefc4fd426e7eb81
Nduwayesu Olivier <oriviernduwayesu@gmail.com>
2026-09-23 16:44:54 +0200

    Fix Swagger API server URL

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const swaggerDefinition = {
     },
     servers: [
         {
-            url: process.env.API_URL || 'http://localhost:3000'
+            url: process.env.API_URL || '/'
         }
     ],
     components: {
